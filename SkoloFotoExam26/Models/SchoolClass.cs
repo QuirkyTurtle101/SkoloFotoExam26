99872c0ca9470ccc175de0a09e815cb7804314fc
akm002-dot <akm002@edu.zealand.dk>
2026-05-07 13:27:39 +0200

    ..

---
+++ SkoloFotoExam26/Models/SchoolClass.cs
@@ -0,0 +1,24 @@
+﻿namespace SkoloFotoExam26.Models
+{
+    public class SchoolClass
+    {
+
+
+        public int SchoolClassID { get; private set; }
+        public string ClassName { get; private set; }
+
+        public School TheSchool { get; private set; }
+
+        public SchoolClass(string className, School theSchool)
+        {
+            ClassName = className;
+            TheSchool = theSchool;
+        }
+
+
+        public override string ToString()
+        {
+            return $"ClassName: {ClassName}";
+        }
+    }
+}
