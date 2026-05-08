327ec9e05d80bfa25d231c53441d0cfa9b864b5e
MidnightSuns <omgzergrushguy@gmail.com>
2026-05-08 12:47:44 +0200

    login repo

---
+++ SkoloFotoExam26/Models/LoginInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace SkoloFotoExam26.Models
+{
+    /// <summary>
+    /// Model for handling login info pulled from the database. Used for LoginRepo.
+    /// </summary>
+    public class LoginInfo
+    {
+        public string Email { get; }
+        public byte[] PasswordHash { get; }
+        public UserType TheUserType { get; }
+
+        public LoginInfo(string email, byte[] passwordHash, UserType theUserType)
+        {
+            Email = email;
+            PasswordHash = passwordHash;
+            TheUserType = theUserType;
+        }
+
+
+    }
+}
