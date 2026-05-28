5de7360e33c6ae49df20c1e821031bddc05fce05
akm002-dot <akm002@edu.zealand.dk>
2026-05-28 10:35:11 +0200

    lærer

--- ConsoleAksel/Program.cs
+++ ConsoleAksel/Program.cs
@@ -2,6 +2,7 @@
 using SkoloFotoExam26.Interfaces;
 using SkoloFotoExam26.Models;
 using SkoloFotoExam26.Services;
+
 Console.WriteLine();
 
 //SchoolSecretary schoolSecretary1 = new SchoolSecretary("F. L.", "FirstName", "LastName", "PhoneNumber", "Email");
@@ -9,4 +10,32 @@ Console.WriteLine();
 //SchoolSecretaryRepo.AddKeyValue(schoolSecretary1.SchoolSecretaryID, schoolSecretary1);
 
 //SchoolSecretaryRepo.PrintAll();
+//PhotoTest photoTest = new PhotoTest();
+//await photoTest.PhotoTestAsync();
+
+
+//public class PhotoTest
+//{
+//    IRepoAsync<Photo, int> _photoRepo;
+
+//    public PhotoTest(IRepoAsync<Photo, int> photoRepo)
+//    {
+//        _photoRepo = photoRepo;
+//    }
+
+//    public PhotoTest()
+//    {
+        
+//    }
+
+//    public async Task PhotoTestAsync()
+//    {
+//        List<Photo> photos = await _photoRepo.GetAllAsync();
+//        foreach (var item in photos)
+//        {
+//            Console.WriteLine(item);
+//        }
+//    }
+//}
+
 
